5d51e5550da3705c56b4c03ed059f2797d553de2
KOBAYASHI Kazuhiro <orihuzakihsayabok@gmail.com>
2023-02-09 10:51:54 +0900

    creusot_sandbox: Setup

---
+++ creusot_sandbox/src/lib.rs
@@ -0,0 +1,4 @@
+#![cfg_attr(
+    not(feature = "contracts"),
+    feature(proc_macro_hygiene, stmt_expr_attributes)
+)]
---
+++ creusot_sandbox/update_mlcfg.sh
@@ -0,0 +1,1 @@
+cargo creusot --output-file mlcfg/creusot_sandbox.mlcfg -- --features contracts
